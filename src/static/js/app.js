f8f87a9daaee998a43b53dc62efd89de5db4d34d
shubhamGhanghotia1 <shubhamghanghotia.yt@gmail.com>
2024-04-04 10:29:22 +0530

    updated item name

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add'}
+                        {submitting ? 'Adding...' : 'Add Item to test'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
